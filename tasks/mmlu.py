c1b93fbaf594df0727e07158dec4d8f31df5b723
Marlos Bosso <marlosbosso@outlook.com>
2026-02-25 09:45:19 -0300

    change mmlu dataset to the translated version

--- tasks/mmlu.py
+++ tasks/mmlu.py
@@ -1,6 +1,6 @@
 """
-The MMLU dataset.
-https://huggingface.co/datasets/cais/mmlu
+The translated into Portuguese version of the MMLU dataset.
+https://huggingface.co/datasets/marlosb/mmlu-pt
 """
 
 from datasets import load_dataset
@@ -19,7 +19,7 @@ class MMLU(Task):
             assert split == "train", "auxiliary_train must be split into train"
         self.subset = subset
         self.split = split
-        self.ds = load_dataset("cais/mmlu", subset, split=split).shuffle(seed=42)
+        self.ds = load_dataset("marlosb/mmlu-pt", subset, split=split).shuffle(seed=42)
         if subset == "auxiliary_train":
             # I don't understand why but the auxiliary_train rows have some weird additional 'train' wrapper
             self.ds = self.ds.map(lambda row: row['train'], remove_columns=['train'])
